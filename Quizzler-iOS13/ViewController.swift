25f11458ad22f3e9c89c44522aeb30e13a17e899
brGuirra <br.guirra@gmail.com>
2022-01-30 18:20:05 -0300

    feat: add a timer to show to user if the answer is correct
    The color of the tapped button changes to show if the answer
    is correct or wrong, the timer clears the color after 0.5
    seconds

--- Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13/ViewController.swift
@@ -14,10 +14,21 @@ class ViewController: UIViewController {
     @IBOutlet var falseButton: UIButton!
     @IBOutlet var progressBar: UIProgressView!
     
+    var timer = Timer()
+    
     let quiz = [
-        Question(text: "Four + Two is equal to Six", answer: "True"),
-        Question(text: "Five - Three is greater than One", answer: "True"),
-        Question(text: "Three + Eight is less than Ten", answer: "False")
+        Question(q: "A slug's blood is green.", a: "True"),
+        Question(q: "Approximately one quarter of human bones are in the feet.", a: "True"),
+        Question(q: "The total surface area of two human lungs is approximately 70 square metres.", a: "True"),
+        Question(q: "In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.", a: "True"),
+        Question(q: "In London, UK, if you happen to die in the House of Parliament, you are technically entitled to a state funeral, because the building is considered too sacred a place.", a: "False"),
+        Question(q: "It is illegal to pee in the Ocean in Portugal.", a: "True"),
+        Question(q: "You can lead a cow down stairs but not up stairs.", a: "False"),
+        Question(q: "Google was originally called 'Backrub'.", a: "True"),
+        Question(q: "Buzz Aldrin's mother's maiden name was 'Moon'.", a: "True"),
+        Question(q: "The loudest sound produced by any animal is 188 decibels. That animal is the African Elephant.", a: "False"),
+        Question(q: "No piece of square dry paper can be folded in half more than 7 times.", a: "False"),
+        Question(q: "Chocolate affects a dog's heart and nervous system; a few ounces are enough to kill a small dog.", a: "True")
     ]
     
     var questionNumber = 0
@@ -30,13 +41,19 @@ class ViewController: UIViewController {
     
 
     @IBAction func awswerButtonPressed(_ sender: UIButton) {
+        timer.invalidate()
+        
         let userAnswer = sender.currentTitle
         let actualAnswer = quiz[questionNumber].answer
         
         if userAnswer == actualAnswer {
-            print("Right!")
+            sender.backgroundColor = #colorLiteral(red: 0.1803921569, green: 0.6901960784, blue: 0.5254901961, alpha: 1)
+            sender.layer.borderColor = #colorLiteral(red: 0.2745098039, green: 0.3921568627, blue: 0.5647058824, alpha: 1)
+            sender.layer.cornerRadius = 28
         } else {
-            print("Wrong!")
+            sender.backgroundColor = #colorLiteral(red: 0.8549019608, green: 0, blue: 0.2156862745, alpha: 1)
+            sender.layer.borderColor = #colorLiteral(red: 0.2745098039, green: 0.3921568627, blue: 0.5647058824, alpha: 1)
+            sender.layer.cornerRadius = 28
         }
         
         if questionNumber < (quiz.count - 1) {
@@ -45,11 +62,17 @@ class ViewController: UIViewController {
             questionNumber = 0
         }
         
-        updateUI()
+        timer = Timer.scheduledTimer(withTimeInterval: 0.5, repeats: false) {
+           [weak self] timer in
+            
+            self?.updateUI()
+        }
     }
     
     func updateUI() {
         questionLabel.text = quiz[questionNumber].text
+        trueButton.backgroundColor = UIColor.clear
+        falseButton.backgroundColor = UIColor.clear
     }
     
 }
